0a387fda9c2a05864a4b91f997d982b335a699dd
LorenzoBondan <lbbondan@gmail.com>
2023-02-24 14:48:17 -0300

    project finished

---
+++ frontweb/src/pages/Admin/Employees/List/index.tsx
@@ -0,0 +1,68 @@
+import './styles.css';
+
+import Pagination from 'components/Pagination';
+import EmployeeCard from 'components/EmployeeCard';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Employee } from 'types/employee';
+import { AxiosRequestConfig } from 'axios';
+import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from 'util/requests';
+import { hasAnyRoles } from 'util/auth';
+
+const List = () => {
+
+  const [page, setPage] = useState<SpringPage<Employee>>();
+
+  useEffect(() => {
+    handlePageChange(0);
+  }, []);
+
+  const handlePageChange = (pageNumber: number) => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/employees',
+      params: {
+        page: pageNumber,
+        size: 10,
+      },
+      withCredentials: true
+    }; 
+    requestBackend(config)
+      .then((response) => {
+        setPage(response.data);
+        console.log(response.data);
+      });
+  }
+
+  return (
+    <>
+    {hasAnyRoles(['ROLE_ADMIN']) ? (
+        <Link to="/admin/employees/create">
+          <button className="btn btn-primary text-white btn-crud-add">
+            ADICIONAR
+          </button>
+        </Link>
+      ) : (
+        <></>
+      )
+    }
+    
+    {page?.content.map((employee) => (
+            <div key={employee.id}>
+                <EmployeeCard employee={employee} />
+            </div>
+          ))
+      }
+
+      <Pagination
+        forcePage={0}
+        pageCount={page ? page.totalPages : 0}
+        range={3}
+        onChange={handlePageChange}
+      />
+    </>
+  );
+};
+
+export default List;
